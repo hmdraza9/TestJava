dae2f1b782167f8e05b6eb3f6ef09f7c6001f92c
hmdraza9 <hmdraza9@gmail.com>
2020-07-28 15:41:06 +0530

    periodic sync

---
+++ src/testOne/setClassEx.java
@@ -0,0 +1,17 @@
+package testOne;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class setClassEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		Set<Integer> s = new HashSet<Integer>();
+		TreeSet sorted = new TreeSet<Integer>();
+	}
+
+}
