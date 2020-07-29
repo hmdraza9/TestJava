d2acc4afe9cbd273312d27abad6a6e6600534353
hmdraza9 <hmdraza9@gmail.com>
2020-07-30 02:20:15 +0530

    periodic sync

--- src/javaConcepts/Hello.java
+++ src/javaConcepts/Hello.java
@@ -11,5 +11,12 @@ public class Hello {
 	      System.out.println(obj.name);
 //	      DataTable arg1 = null;
 //	      List<String> data = arg1.
+//	      list of functional interfaces
+//	      Comparable f = new Comparable();
+//	      Runnable er
+//	      ActionListener cc
+	      String str1="AH e l l  o";
+	      String str2="AAH e l l o";
+	      System.out.println(str1.compareTo(str2));
 	   }
 	}
---
+++ src/testSelenium/WallpaperDownloadClass.java
@@ -0,0 +1,65 @@
+package testSelenium;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import uTilities.ScreenshotTaker;
+
+public class WallpaperDownloadClass {
+
+	static WebDriver driver;
+	static ScreenshotTaker st;
+
+	public static void main(String[] args) throws IOException, StaleElementReferenceException {
+		// TODO Auto-generated method stub
+		st = new ScreenshotTaker();
+		List<WebElement> elList;
+		List<String> elTextList;
+		String url = "https://wonderfulengineering.com/biggest-collection-of-hd-baby-wallpaper-for-desktop-and-mobile/amp/";
+		System.setProperty("webdriver.chrome.driver",
+				"C:/Users/Abdul Hamid Raza/Documents/all-drivers/chromedriver.exe");
+
+		ChromeOptions option = new ChromeOptions();
+		option.addArguments("start-maximized");
+		option.addArguments("--headless");
+		driver = new ChromeDriver(option);
+
+		driver.get(url);
+		elList = new ArrayList<WebElement>();//
+		elList = driver.findElements(By.xpath("//*[contains(@src,'http://wonderfulengineering.com/wp-content/')]"));
+		elTextList = new ArrayList<String>();
+		for (WebElement el : elList) {
+			elTextList.add(el.getAttribute("src"));
+		}
+
+		for (int i = 0; i < elTextList.size(); i++) {
+			String tempElText = elTextList.get(i).toString();
+			int tempElTextLength = tempElText.length();
+			System.out.println(tempElText);
+			String tempUrl = tempElText;
+			if (tempElText.endsWith(".jpg")
+					&& ((tempElText.substring(tempElTextLength - 12, tempElTextLength).contains("x")))) {
+				tempUrl = tempElText.substring(0, tempElTextLength - 12)
+						+ tempElText.substring(tempElTextLength - 4, tempElTextLength);
+			} else if (tempElText.endsWith(".jpeg")
+					&& ((tempElText.substring(tempElTextLength - 13, tempElTextLength).contains("x")))) {
+				tempUrl = tempElText.substring(0, tempElTextLength - 13)
+						+ tempElText.substring(tempElTextLength - 5, tempElTextLength);
+			}
+			System.out.println(tempUrl);
+			driver.get(tempUrl);
+			st.Snapper(driver);
+		}
+
+		driver.quit();
+	}
+
+}
